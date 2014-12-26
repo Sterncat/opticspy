36f0397fe47656c537f1ff65bf99c459db0268f2
Marvin <Alex.Fanxing@gmail.com>
2014-12-26 00:04:18 -0600

    12.26
    修改core
    修改diffraction模块

--- diffraction.py
+++ diffraction.py
@@ -1,5 +1,5 @@
-from core import diffraction_core
-__all__ = ['apershow','rec','circle']
+from core import diff_core as DC
+#__all__ = ['apershow','rec','circle']
 
 
 def apershow(aper_fft, clipfactor):
@@ -11,12 +11,16 @@ def apershow(aper_fft, clipfactor):
 	print gamin
 #	scale=0.25*255*clipfactor/gamax
 #	ga=min(scale*aper_fft,255);
-	plt.imshow(aper_fft,vmax=gamax, vmin=gamin)
-	plt.show()
+	DC.plt.imshow(aper_fft,vmax=gamax, vmin=gamin)
+	DC.plt.show()
 	
 def rec(background_size, rec_height, rec_width, row_start, col_start, clipfactor):
 	"""
 	Compute the rectangle aperture's diffraction pattern.
+	
+	Example
+	-----------
+	diffraction.rect(200,20,20,80,80,1)
 	
  	Parameters
 	-----------
@@ -42,31 +46,47 @@ def rec(background_size, rec_height, rec_width, row_start, col_start, clipfactor
 	colstart = col_start
 	clipfactor = clipfactor
 	matrix_1 = [height,width]
-	aper = np.ones(matrix_1)
-	aper1 = np.zeros([n,n])
+	aper = DC.np.ones(matrix_1)
+	aper1 = DC.np.zeros([n,n])
 	aper1[(rowstart-1):(rowstart-1+height),(colstart-1):(colstart-1+width)] = aper
-	print "aperture_matrix:",aper1
-	aperfft = np.fft.fftshift(np.fft.fft2(aper1))
+#	print "aperture_matrix:",aper1
+	aperfft = DC.np.fft.fftshift(DC.np.fft.fft2(aper1))
 	apershow(aperfft,1)
 	
 	
 	
 
 def circle(background_size, center, radius, clipfactor):
+	"""
+	Compute the cirlce aperture's diffraction pattern.
+	
+	Example
+	-----------
+	diffraction.circle(200,[50,50],20,1)
+	
+ 	Parameters
+	-----------
+	background_size: int
+				Square background
+	center: int
+				aperture center
+	radius: int
+				aperture radius
+				
+	returns
+	-----------
+	out: aperture's matrix and figure of diffraction pattern
+	"""
+
 	n = background_size
-	xcenter = center[0]+n/2
-	ycenter = center[1]+n/2
 	radius = radius
 	clipfactor = clipfactor
-	print n, xcenter, ycenter, radius, clipfactor
-#	matrix_1 = [height,width]
-#	aper = np.ones(matrix_1)
-	aper1 = np.zeros([n,n])
+	aper1 = DC.np.zeros([n,n])
 	for i in range(n):
 		for j in range(n):
-			r = sqrt((i-xcenter)**2+(j-ycenter)**2)
+			r = DC.sqrt((i-background_size/2)**2+(j-background_size/2)**2)
 			if r < radius:
 				aper1[i,j] = 1
-	print "aperture_matrix:\n",aper1
-	aperfft = np.fft.fftshift(np.fft.fft2(aper1))
+#	print "aperture_matrix:",aper1
+	aperfft = DC.np.fft.fftshift(DC.np.fft.fft2(aper1))
 	apershow(aperfft,1)
