e238e5d105e837e2c55673cd8c09f39d8e4cd86b
stern <alex.fanxing@gmail.com>
2015-02-15 02:10:04 -0500

    interferometer aper

--- pltshow.py
+++ pltshow.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
+# def apershow(obj):
+# 	obj = -abs(obj)
+# 	gamax=obj.max()
+# 	gamin=obj.min()
+# 	print gamax
+# 	print gamin
+# 	plt.imshow(obj,vmax=gamax, vmin=gamin)
+# 	plt.set_cmap('Greys')
+# 	plt.show()
 def apershow(obj):
-	obj = abs(obj)
-	gamax=obj.max()
-	gamin=obj.min()
-	print gamax
-	print gamin
-	plt.imshow(obj,vmax=gamax, vmin=gamin)
+	obj = -abs(obj)
+	plt.imshow(obj)
+	plt.set_cmap('Greys')
 	plt.show()
