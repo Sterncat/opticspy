4b8b843e1844e66ec2242ad2ebd4b33211774fa4
stern <alex.fanxing@gmail.com>
2015-04-20 12:41:42 -0400

    initial

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+"""
+ *       ___            _     _                              
+ *      / _ \   _ __   | |_  (_)   ___   ___   _ __    _   _ 
+ *     | | | | | '_ \  | __| | |  / __| / __| | '_ \  | | | |
+ *     | |_| | | |_) | | |_  | | | (__  \__ \ | |_) | | |_| |
+ *      \___/  | .__/   \__| |_|  \___| |___/ | .__/   \__, |
+ *             |_|                            |_|      |___/ 
+ *
+"""
+
 import aper, aberration, interferometer_seidel,interferometer_zenike, zernike, test
 import warnings as __warnings__
 __warnings__.filterwarnings("ignore")
