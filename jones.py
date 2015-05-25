9286300092d944c651e4fc62096638d9b0178863
stern <alex.fanxing@gmail.com>
2015-05-25 01:31:38 -0400

    add simple jones class

--- jones.py
+++ jones.py
@@ -2,13 +2,13 @@
 see http://en.wikipedia.org/wiki/Jones_calculus
 currently does not normalize the output
 """
-import numpy as np
-from numpy import pi
+import numpy as __np__
+from numpy import pi as __pi__
 
 def rotator(angle):
-    s = np.sin(angle)
-    c = np.cos(angle)
-    rotator = np.matrix([[c,-s],[s,c]])
+    s = __np__.sin(angle)
+    c = __np__.cos(angle)
+    rotator = __np__.matrix([[c,-s],[s,c]])
 
 class Component(object):
     """Base Class for Waveplates and Filters"""
@@ -31,15 +31,15 @@ class Component(object):
 class HalfWavePlate(Component):
     """Subclassed special case 1: HWP"""
     def __init__(self):
-        self.matrix = np.matrix([[1,0],[0,-1]])
+        self.matrix = __np__.matrix([[1,0],[0,-1]])
         
 class QuaterWavePlate(Component):
     def __init__(self):
-        self.matrix = np.matrix([[1,0],[0,1j]])
+        self.matrix = __np__.matrix([[1,0],[0,1j]])
         
 class Birefringence(Component):
     def __init__(self, w1, w2):
-        self.matrix = np.matrix([[np.exp(1j*w1),0],[0,np.exp(1j*w2)]])
+        self.matrix = __np__.matrix([[__np__.exp(1j*w1),0],[0,__np__.exp(1j*w2)]])
         
         
 
@@ -57,25 +57,25 @@ class Jones(object):
         
 class Hpol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[1],[0]])
+        self.vector=__np__.matrix([[1],[0]])
 class Vpol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[0],[1]])    
+        self.vector=__np__.matrix([[0],[1]])    
         
 class D1pol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[1],[1]]) 
+        self.vector=__np__.matrix([[1],[1]]) 
 class D2pol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[1],[-1]]) 
+        self.vector=__np__.matrix([[1],[-1]]) 
         
 class C1pol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[1],[1j]]) 
+        self.vector=__np__.matrix([[1],[1j]]) 
 class C2pol(Jones):
     def __init__(self):
-        self.vector=np.matrix([[1],[-1j]]) 
+        self.vector=__np__.matrix([[1],[-1j]]) 
 
 if __name__ == "__main__":
     #Usage example
-    print QuaterWavePlate().rotate(pi/4)*HalfWavePlate().rotate(pi/8)*D1pol()
+    print QuaterWavePlate().rotate(__pi__/4)*HalfWavePlate().rotate(__pi__/8)*D1pol()
