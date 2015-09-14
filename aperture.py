1a7fed3ebe36b38bc8604e6da8245d70c8881122
stern <alex.fanxing@gmail.com>
2015-09-14 00:58:27 -0400

    aperture and diffraction

--- aperture.py
+++ aperture.py
@@ -29,15 +29,27 @@ class Aperture():
 
 	def fresnel(self,z = 2,lambda1 = 660*10**(-9)):
 		"""
-		Compute the cirlce aperture's diffraction pattern.
+		Compute the fresnel diffraction pattern.
 
 		Output
 		------------------------------------
 		Diffraction pattern figure
 
 		"""
-		print "---------diffraction----------"
+		print "---------Fresnel Diffraction----------"
 		__diffraction__.fresnel(self,z,lambda1)
+		return 0
+	def fraunhofer(self,z = 2,lambda1 = 660*10**(-9)):
+		"""
+		Compute the frauhofer diffraction pattern.
+
+		Output
+		------------------------------------
+		Diffraction pattern figure
+
+		"""
+		print "---------Fraunhofer Diffraction----------"
+		__diffraction__.fraunhofer(self,z,lambda1)
 		return 0
 
 	def otf(self):
@@ -88,7 +100,7 @@ class Circle(Aperture):
 class Rectangle(Aperture):
 	def __init__(self, background=500, height=200, width=200, scale=0.01/200):
 		"""
-		Build a  rectangle aperture example
+		Build a rectangle aperture instance
 		
 		Example
 		-----------
@@ -104,18 +116,43 @@ class Rectangle(Aperture):
 					aperture width
 		"""
 		self.__type__ = 'rectangle'
-		self.__background__ = background
+		n = self.__background__ = background
 		self.__height__ = height
 		self.__width__ = width
 		self.__scale__ = scale
-		n = background
-		matrix_1 = [height,width]
-		aper1 = __np__.ones(matrix_1)
+		#matrix_1 = [height,width]
+		aper1 = __np__.ones([height,width])
 		self.__aper__ = __np__.zeros([n,n])
 		self.__aper__[(n/2-height/2):(n/2-height/2+height),(n/2-width/2):(n/2-width/2+width)] = aper1
 
+class DoubleRectangle(Aperture):
+	def __init__(self, background=500, height=50, width=2, separation=4, scale=0.01/200):
+		"""
+		Build a DoubleRectangle aperture instance, could use as a doubleslit aperture
+		"""
+		self.__type__ = "doubleRectangle"
+		n = self.__background__ = background
+		self.__height__ = height
+		self.__width__ = width
+		self.__separation__ = separation
+		self.__scale__ = scale
+		self.__aper__ = __np__.zeros([n,n])
+		aper1 = __np__.ones([height,width])
+		self.__aper__[(n/2-height/2):(n/2-height/2+height),(n/2-width/2-separation/2):(n/2-width/2+width-separation/2)] = aper1
+		self.__aper__[(n/2-height/2):(n/2-height/2+height),(n/2-width/2+separation/2):(n/2-width/2+width+separation/2)] = aper1
 
-
+class Frame(Aperture):
+	def __init__(self, background=500, outside=200, inside=100, scale=0.01/200):
+		self.__type__ = "frame"
+		n = self.__background__ = background
+		self.__outside__ = outside
+		self.__inside__ = inside
+		self.__scale__ = scale
+		self.__aper__ = __np__.zeros([n,n])
+		aper1 = __np__.ones([outside,outside])
+		aper2 = __np__.zeros([inside,inside])
+		self.__aper__[(n/2-outside/2):(n/2-outside/2+outside),(n/2-outside/2):(n/2-outside/2+outside)] = aper1
+		self.__aper__[(n/2-inside/2):(n/2-inside/2+inside),(n/2-inside/2):(n/2-inside/2+inside)] = aper2
 
 
 
--- diffraction.py
+++ diffraction.py
@@ -7,6 +7,7 @@ def __apershow__(obj):
 	__plt__.set_cmap('Greys')
 	__plt__.show()
 def fresnel(aperture,z = 2,lambda1 = 660*10**(-9)):
+	aperturelist = ['rectangle','doubleRectangle','frame']
 	if aperture.__type__ == 'circle':
 		n = aperture.__background__
 		d = aperture.__d__
@@ -14,11 +15,12 @@ def fresnel(aperture,z = 2,lambda1 = 660*10**(-9)):
 		scale = aperture.__scale__
 		Nf = D**2/(4*lambda1*z);  #Fresnel number. Smaller is better for single-DFT Fresnel
 		print "Fresnel number = ", Nf
-	elif aperture.__type__ == 'rectangle':
+	elif aperture.__type__ in aperturelist:
+		print aperture.__type__
 		n = aperture.__background__
 		scale = aperture.__scale__
 	else:
-		print "12324533241231231232131"
+		print "No this kind aperture for fresnel diffraction"
 
 	x1 = __np__.linspace(-n/2+1,n/2,n)
 	[x,y] = __np__.meshgrid(x1,x1)
@@ -28,6 +30,15 @@ def fresnel(aperture,z = 2,lambda1 = 660*10**(-9)):
 	__apershow__(diffraction)
 	return diffraction
 
+
+
+def fraunhofer(aperture, z = 2, lambda1 = 660*10**(-9)):
+	"""
+	Fraunhofer diffraction
+	"""
+	diffraction = 1j*__np__.exp(1j*2*__np__.pi/lambda1*z)/lambda1/z*__np__.fft.fftshift(__np__.fft.fft2(aperture.__aper__))
+	__apershow__(diffraction)
+	return 0
 
 	
 
