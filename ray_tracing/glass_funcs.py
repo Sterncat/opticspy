52aaf91fe8c74f3a0aa80031634bac2c4e16c6d5
stern <alex.fanxing@gmail.com>
2016-02-26 21:51:27 -0500

    update

--- ray_tracing/glass_funcs.py
+++ ray_tracing/glass_funcs.py
@@ -5,80 +5,33 @@ import os
 from glass_function.refractiveIndex import *
 
 
-#glass related functions
-
-
-# def glass2indexlist(wavelength_list,glassname):
-	
-# 	lens_index_list = []
-# 	wavelength_num = len(wavelength_list)
-# 	if glassname == 'air' or glassname == 'AIR':
-# 		lens_index_list = [1]*wavelength_num
-# 		return lens_index_list
-# 	else:
-# 		n = glassname.find('_')
-# 		glass_catalog_name = glassname[n+1:]
-# 		glass_name = glassname[:n]
-# 		catalog = RefractiveIndex()
-# 		for w in wavelength_list:
-# 			mat = catalog.getMaterial('glass', glass_catalog_name, glass_name)
-# 			n = mat.getRefractiveIndex(w)
-# 			lens_index_list.append(round(n,6))
-# 	return lens_index_list
-
-
+# glass related functions
+# use polyanskiy's refractiveindex database(http://refractiveindex.info/) 
+# polyanskiy's github: https://github.com/polyanskiy
+# https://github.com/polyanskiy/refractiveindex.info-database
+# Also use Pavel Dmitriev's script for return refractiveindex from database
+# https://github.com/kitchenknif/PyTMM
 
 
 def glass2indexlist(wavelength_list,glassname):
-	"""
-	convert glass to index list related to wavelength
-	glassname: str
-	"""
-	#print 'glass',glassname
+	
+	lens_index_list = []
 	wavelength_num = len(wavelength_list)
 	if glassname == 'air' or glassname == 'AIR':
 		lens_index_list = [1]*wavelength_num
-		#output(wavelength_list,lens_index_list)
 		return lens_index_list
 	else:
 		n = glassname.find('_')
 		glass_catalog_name = glassname[n+1:]
 		glass_name = glassname[:n]
-		dir = 'opticspy/ray_tracing/glass/' + glass_catalog_name+'/'+glassname
-		e = os.path.exists(dir)
+		catalog = RefractiveIndex()
+		for w in wavelength_list:
+			mat = catalog.getMaterial('glass', glass_catalog_name, glass_name)
+			n = mat.getRefractiveIndex(w)
+			lens_index_list.append(round(n,6))
+	return lens_index_list
 
-		if e == False:
-			print 'No This Kind Of Glass'
-			return None
-		else:
-			wave_list = []
-			index_list = []
-			lens_index_list = []
-			file = open(dir)
-			while 1:
-				line = file.readline()
-				if not line:
-					break
-				a = line.split()
-				wave_list.append(float(a[0]))
-				index_list.append(float(a[1]))
-			file.close()
-			for wavelength in wavelength_list:
-				index = find_closest_wavelength(wavelength,wave_list,index_list)
-				lens_index_list.append(index)
-			#output(wavelength_list,lens_index_list)
-			return lens_index_list
-
-def find_closest_wavelength(wavelength,wave_list,index_list):
-	n = 0
-	for i in wave_list:
-		if wavelength == i:
-			return index_list[n]
-		elif wavelength < i:
-			return (index_list[n]+index_list[n])/2
-		else:
-			n = n + 1
-
+	
 def output(wavelength_list,lens_index_list):
 	print 'Lens wavelength vs index'
 	print 'wavelength-----index---'
@@ -88,12 +41,56 @@ def output(wavelength_list,lens_index_list):
 	print '-----------------------'
 	return 0
 
+#=============================================================
+# Old glass check functions
+#
+# def glass2indexlist(wavelength_list,glassname):
+# 	"""
+# 	convert glass to index list related to wavelength
+# 	glassname: str
+# 	"""
+# 	#print 'glass',glassname
+# 	wavelength_num = len(wavelength_list)
+# 	if glassname == 'air' or glassname == 'AIR':
+# 		lens_index_list = [1]*wavelength_num
+# 		#output(wavelength_list,lens_index_list)
+# 		return lens_index_list
+# 	else:
+# 		n = glassname.find('_')
+# 		glass_catalog_name = glassname[n+1:]
+# 		glass_name = glassname[:n]
+# 		dir = 'opticspy/ray_tracing/glass/' + glass_catalog_name+'/'+glassname
+# 		e = os.path.exists(dir)
 
+# 		if e == False:
+# 			print 'No This Kind Of Glass'
+# 			return None
+# 		else:
+# 			wave_list = []
+# 			index_list = []
+# 			lens_index_list = []
+# 			file = open(dir)
+# 			while 1:
+# 				line = file.readline()
+# 				if not line:
+# 					break
+# 				a = line.split()
+# 				wave_list.append(float(a[0]))
+# 				index_list.append(float(a[1]))
+# 			file.close()
+# 			for wavelength in wavelength_list:
+# 				index = find_closest_wavelength(wavelength,wave_list,index_list)
+# 				lens_index_list.append(index)
+# 			#output(wavelength_list,lens_index_list)
+# 			return lens_index_list
 
+# def find_closest_wavelength(wavelength,wave_list,index_list):
+# 	n = 0
+# 	for i in wave_list:
+# 		if wavelength == i:
+# 			return index_list[n]
+# 		elif wavelength < i:
+# 			return (index_list[n]+index_list[n])/2
+# 		else:
+# 			n = n + 1
 
-
-
-
-
-
-	
