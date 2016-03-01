d261ff33544922747db3609ab5f093cde8f67bc3
stern <alex.fanxing@gmail.com>
2016-03-01 18:26:57 -0500

    draw

--- ray_tracing/draw.py
+++ ray_tracing/draw.py
@@ -6,32 +6,89 @@ import numpy as np
 import trace
 # All lens drawing and ray drawing functions
 
-def draw_surface(r,x0,d):
+def draw_surface(r,x0,d1,d2):
+    '''
+    d1: diameter that rays go through
+    d2: max diameter for a lens, doublet, let edge be flat
+    '''
     verts_1 = []
     verts_2 = []
     codes = []
-    for y in np.linspace(0,d/2,10):
+    for y in np.linspace(0,d1/2,10):
         if r > 0:
             x = -np.sqrt(r**2-y**2) + r
         else:
             x = np.sqrt(r**2-y**2) + r
         verts_1.append([x+x0,y])
         verts_2.append([x+x0,-y])
-        
-    verts = verts_1[::-1] + verts_2[1:]
+    if d1 == d2:
+        verts = verts_1[::-1] + verts_2[1:]
+    elif d1 < d2:
+        verts = [[verts_1[-1][0],d2/2]] + verts_1[::-1] + verts_2[1:] + [[verts_2[-1][0],-d2/2]]
     codes.append(Path.MOVETO)
     for j in range(len(verts)-1):
         codes.append(Path.LINETO)
     return verts,codes,[verts[0],verts[-1]]
 
+
+
 def draw_system(Lens):
     print '------------------start drawing lens system-------------'
-    fig = plt.figure(figsize=(12, 6))
-    fig.canvas.set_window_title('View Lens')
-    fig.suptitle('View Lens:'+Lens.lens_name, fontsize="x-large")
-    ax = fig.add_subplot(111)
+    # adjust diameter
     surface_list = Lens.surface_list
     m = len(surface_list)
+    label_list = []
+    label_list1 = []
+    for num in range(m):
+        if num == 0:
+            if surface_list[num].glass != ('air' or 'AIR'):
+                label_list.append(1)
+            else:
+                label_list.append(0)
+        else:
+            if (surface_list[num-1].glass == ('air' or 'AIR')) and (surface_list[num].glass != ('air' or 'AIR')):
+                label_list.append(1)
+            elif (surface_list[num-1].glass != ('air' or 'AIR')) and (surface_list[num].glass != ('air' or 'AIR')):
+                label_list.append(1)
+            elif (surface_list[num-1].glass != ('air' or 'AIR')) and (surface_list[num].glass == ('air' or 'AIR')):
+                label_list.append(2)
+            else:
+                label_list.append(0)
+
+    label_list1.append([])
+    for num in range(m):
+        n = len(label_list1)
+        if label_list[num] == 0:
+            label_list1[n-1].append(num)
+            label_list1.append([])
+            n = n + 1
+        elif label_list[num] == 1:
+            label_list1[n-1].append(num)
+        else:
+            label_list1[n-1].append(num)
+            label_list1.append([])
+            n = n + 1
+    label_list1.pop()
+
+    draw_diameter_list = [0]*len(surface_list)
+    max_diameter_list = []
+    for unit_list in label_list1:
+        max_d = 0
+        for s_index in unit_list:
+            if surface_list[s_index].__diameter__ >= max_d:
+                max_d = surface_list[s_index].__diameter__
+        max_diameter_list.append(max_d)
+
+    k = -1
+    for unit_list in label_list1:
+        k = k + 1
+        for s_index in unit_list:
+            draw_diameter_list[s_index] = max_diameter_list[k]
+
+    fig = plt.figure(figsize=(12, 6))
+    fig.canvas.set_window_title('View Lens')
+    fig.suptitle('View Lens: '+Lens.lens_name, fontsize="x-large")
+    ax = fig.add_subplot(111)
     thinkness_list = []
     start_end_list = []
     verts_list = []
@@ -41,20 +98,21 @@ def draw_system(Lens):
         glass = surface_list[num].glass
         t = surface_list[num].thickness
         thinkness_list.append(t)
-        d = surface_list[num].__diameter__
+        d1 = surface_list[num].__diameter__
+        d2 = draw_diameter_list[num]
 
         if num == 0:
             x0 = 0 - thinkness_list[0]
         else:
             x0 = sum(thinkness_list[0:-1]) - thinkness_list[0]
             #print x0
-        verts,codes,start_end = draw_surface(r,x0,d)
+        verts,codes,start_end = draw_surface(r,x0,d1,d2)
         start_end_list.append(start_end)
         verts_list.append(verts)
         codes_list.append(codes)
 
         # start drawing surface
-        print 'draw surface:',num+1
+        #print 'draw surface:',num+1
         path = Path(verts, codes)
         patch = patches.PathPatch(path, facecolor='none', lw=1)
         ax.add_patch(patch)
@@ -63,7 +121,7 @@ def draw_system(Lens):
         if num == 0:
             pass
         elif surface_list[num-1].glass != ('air' or 'AIR'):
-            print 'drawing edge:',num,'---',num+1
+            #print 'drawing edge:',num,'---',num+1
             verts1 = [start_end_list[num-1][0],start_end_list[num][0]]
             #print verts1
             verts2 = [start_end_list[num-1][1],start_end_list[num][1]]
@@ -86,20 +144,10 @@ def draw_system(Lens):
         patch1 = patches.PathPatch(path, facecolor='none',fill=0, lw=1,edgecolor=linecolor)
         ax.add_patch(patch1)
     ax.set_xlim(-0.5*sum(thinkness_list[1:-1]),1.1*sum(thinkness_list[1:-1]))
+    d = max(draw_diameter_list)
     ax.set_ylim(-d/4*3,d/4*3)
     plt.show()
 
-
-# def draw_rays(Lens,fields,wave_num,ray):
-#   'drawing chef ray and two maginal rays'
-#   'fields = [1,2,3]'
-#   path_list = []
-#   ray_tracing = []
-#   for field in fields:
-#       ray_tracing = trace.trace_one_ray(Lens,field,wave_num,[0,1])
-#       p = draw_line(Lens,ray_tracing)
-#       path_list.append(p)
-#   return path_list
 
 def draw_rays(Lens):
     '''
